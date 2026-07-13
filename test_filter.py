0713f3bec9476bb4d9cf34c97bb87290659170ab
Viktoriia Zagorodnikova <134017362+viktoriia-zagorodnikova@users.noreply.github.com>
2026-07-13 13:29:31 -0500

    Update test_filter.py

--- test_filter.py
+++ test_filter.py
@@ -5,32 +5,26 @@ import time
 
 FILTER_URL = "https://excursium.com/ekskursii-dlya-shkolnikov/list"
 
-class TestFilter:
 
+class TestFilter:
     def test_filters_open_by_default(self, driver):
-        """EX-014: Все фильтры открыты по умолчанию"""
+        """EX-014: All filters are open by default"""
         driver.get(FILTER_URL)
         wait = WebDriverWait(driver, 10)
-
         filter_form = wait.until(EC.presence_of_element_located(
             (By.ID, "filter")))
-
         assert filter_form.is_displayed()
 
     def test_filter_shows_correct_results(self, driver):
-        """EX-017: Фильтр показывает корректные результаты"""
+        """EX-017: Filter shows correct results"""
         driver.get(FILTER_URL)
         wait = WebDriverWait(driver, 10)
-
         wait.until(EC.presence_of_element_located((By.ID, "filter")))
         time.sleep(2)
-
-        # Кликаем через JavaScript на скрытый label
+        # Click via JavaScript on a hidden label
         label = driver.find_element(By.CLASS_NAME, "form-check-label")
         driver.execute_script("arguments[0].click();", label)
-
         time.sleep(2)
-
         results = driver.find_elements(
             By.XPATH, "//div[contains(@class,'card')]")
         assert len(results) > 0
